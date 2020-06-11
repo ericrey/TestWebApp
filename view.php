e4dc608942703fd4a985563ab02ec4fd23dfc476
ericrey <52686016+ericrey@users.noreply.github.com>
2020-06-11 14:45:37 +0800

    Update view.php

--- view.php
+++ view.php
@@ -9,6 +9,24 @@
   {
     die("Error connection: ".sqlsrv_errors());
   }
-  echo "Connection Success";
+//   echo "Connection Success";
+//display table
+echo "<table border= '1'";
+$sql= "SELECT * FROM [dbo].[restaurant]";
+$getResults= sqlsrv_query($conn, $tsql);
+if ($getResult == FALSE)
+{
+  die(sqlsrv_errors());
+}
+while ($row = sqlsrv_fetch_array($getResult, SQLSRV_FETCH_ASSOC))
+{
+  echo "<tr>";
+  echo "<td>". $row['restaurant_id'] . "</td>";
+  echo "<td>". $row['restaurant_name'] ."</td>";
+  echo "<td>". $row['restaurant_address'] . "</td>";
+  echo "<td>". $row['restaurant_phone'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
 
 ?>
